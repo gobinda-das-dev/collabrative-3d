36f650da50da607a55fbe8502809a3a59d93a8f1
Gobinda Das <144037976+gobinda-das-dev@users.noreply.github.com>
2024-04-26 15:44:44 +0000

    stop movement and change duration

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const $$ = (e) => document.querySelectorAll(e);
 
 // Under developmenttt
 const togglerBtn = $('.toggler');
-const tl = gsap.timeline({ defaults: {duration: 2, ease: "expo.inOut",}, paused: true });
+const tl = gsap.timeline({ defaults: {duration: 1, ease: "expo.inOut",}, paused: true });
 const w = gsap.getProperty('.can', 'width');
 const wW = window.innerWidth;
 const duration = (wW > 1000) ? 0.7 : (wW > 700) ? 0.5 : 0.4;
@@ -16,13 +16,13 @@ const duration = (wW > 1000) ? 0.7 : (wW > 700) ? 0.5 : 0.4;
 tl
   .to('.orange > h1', { xPercent: 100 })
   .to('.orange > div > img:not(.can)', { xPercent: 150, rotation: 90 }, '<')
-  .to('.orange .can, .orange .clippy', { x: '-4vw' }, '<')
+  // .to('.orange .can, .orange .clippy', { x: '-2vw' }, '<')
   .to('.orange .clippy > div', { x: -w*2 }, '<')
   .to('.orange', { clipPath: 'inset(0 100% 0 0)' }, '<')
 
   .from('.pineapple > h1', { xPercent: -100 }, '<')
   .from('.pineapple > div > img:not(.can)', { xPercent: -150, rotation: -90 }, '<')
-  .from('.pineapple .can, .pineapple .clippy', { x: '4vw' }, '<')
+  // .from('.pineapple .can, .pineapple .clippy', { x: '2vw' }, '<')
   .from('.pineapple .clippy > div', { x: w*2 }, '<')
 
 
