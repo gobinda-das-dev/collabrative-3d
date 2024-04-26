3420b6f3d39cbd0d1dd644694aa3fd042ae93dfd
Gobinda Das <gobinda993720@gmail.com>
2024-04-26 07:56:16 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+const $ = (e) => document.querySelector(e);
+const $$ = (e) => document.querySelectorAll(e);
+
+
+
+
+
+
+// Under development
+const togglerBtn = $('.toggler');
+const tl = gsap.timeline({ defaults: {duration: 2, ease: "expo.inOut",}, paused: true });
+const w = gsap.getProperty('.can', 'width');
+const wW = window.innerWidth;
+const duration = (wW > 1000) ? 0.7 : (wW > 700) ? 0.5 : 0.4;
+
+tl
+  .to('.orange > h1', { xPercent: 100 })
+  .to('.orange > div > img:not(.can)', { xPercent: 150, rotation: 90 }, '<')
+  .to('.orange .can, .orange .clippy', { x: '-4vw' }, '<')
+  .to('.orange .clippy > div', { x: -w*2 }, '<')
+  .to('.orange', { clipPath: 'inset(0 100% 0 0)' }, '<')
+
+  .from('.pineapple > h1', { xPercent: -100 }, '<')
+  .from('.pineapple > div > img:not(.can)', { xPercent: -150, rotation: -90 }, '<')
+  .from('.pineapple .can, .pineapple .clippy', { x: '4vw' }, '<')
+  .from('.pineapple .clippy > div', { x: w*2 }, '<')
+
+
+let count = 1;
+togglerBtn.addEventListener('click', () => {
+  count ? (tl.play(), count--) : (tl.reverse(), count++);
+});
+
